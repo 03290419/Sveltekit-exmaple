4cd5ec4028e6ed9df0ca51ce4e024f8960fba509
hyunjin <alfma123@naver.com>
2024-04-09 09:55:46 +0900

    feat: initialize git

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,8 @@
+<footer>Copyright@2023, keng</footer>
+
+<style>
+    footer{
+        padding: 1rem;
+        background-color: red;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,52 @@
+<script>
+	import Footer from '$lib/components/Footer.svelte'; // $lib == ./src/lib
+	import Header from '../lib/components/Header.svelte';
+	import './styles.css';
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+		<!-- +page 보여주는 부분 -->
+	</main>
+	<Footer/>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
